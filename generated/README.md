ee536ae8bf280c949826c09c4e4288083ecd4721
Joseph Carroll <jdsalingerjr@gmail.com>
2022-10-11 10:58:02 -0700

    Publish json to git

---
+++ generated/README.md
@@ -0,0 +1,3 @@
+# GENERATED CONTENT ONLY
+
+The contents is this folder are generated from their corresponding YAML files under [descriptions](/descriptions). Do not modify any file in this folder as it will be overwritten during the next build.
--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,72 @@
-const { readdir, readFile, writeFile } = require('fs/promises');
-const { basename, extname, join } = require('path');
+const { readdir, readFile, rm, stat, writeFile, mkdir } = require('fs/promises');
+const { basename, extname, join, dirname } = require('path');
 
 const { load: readYaml } = require('js-yaml');
 
-const buildJsonAsset = async (inputfile, dir) => {
-  const filename = basename(inputfile);
+const buildAssets = async (inputitem, basedir, outdir) => {
+  const filename = basename(inputitem);
   const ext = extname(filename);
   if (ext !== '.yaml') {
-    console.warn(`Unknown file type '${ext}' - Skipping file: ${inputfile}`);
+    console.warn(`Unknown file type '${ext}' - Skipping file: ${inputitem}`);
     return;
   }
 
-  console.log(`Building json file from yaml: ${inputfile}`);
+  console.log(`Building json file from yaml: ${inputitem}`);
   const file = filename.slice(0, -ext.length);
-  const yaml = await readFile(inputfile, { encoding: 'utf-8' });
+  const yaml = await readFile(join(basedir, inputitem), { encoding: 'utf-8' });
   const obj = readYaml(yaml);
-  const outputfile = join(dir, `${file}.json`);
-  await writeFile(outputfile, JSON.stringify(obj, null, 2));
+
+  const outfiledir = join(outdir, dirname(inputitem));
+  await ensuredir(outfiledir);
+
+  const outfilepath = join(outfiledir, `${file}.json`);
+  await writeFile(outfilepath, JSON.stringify(obj, null, 2));
+  await writeFile(join(outfiledir, filename), yaml);
+};
+
+const clean = async (outdir) => {
+  const elements = await readdir(outdir, { withFileTypes: true });
+
+  for (const element of elements) {
+    if (element.name === 'README.md') {
+      continue;
+    }
+
+    await rm(join(outdir, element.name), { recursive: true });
+  }
+};
+
+const ensuredir = async (dir) => {
+  try {
+    await stat(dir);
+  } catch (_) {
+    await mkdir(dir);
+  }
 };
 
 const build = async () => {
-  const workQueue = ['descriptions'];
+  const workQueue = [''];
+
+  const basedir = 'descriptions';
+  const outdir = 'generated';
+  await ensuredir(outdir);
+  await clean(outdir);
 
   while (workQueue.length) {
-    const dir = workQueue.shift();
+    const workitem = workQueue.shift();
+    const dir = join(basedir, workitem);
     const elements = await readdir(dir, { withFileTypes: true });
 
     for (const element of elements) {
-      const workItem = join(dir, element.name);
+      const diritem = join(workitem, element.name);
 
       if(element.isDirectory()) {
-        workQueue.push(workItem);
+        workQueue.push(diritem);
       } else {
-        await buildJsonAsset(workItem, dir);
+        await buildAssets(diritem, basedir, outdir);
       }
     }
   }
 };
 
-build();
+build().then(() => console.log('Successfully built assets'), console.error);
